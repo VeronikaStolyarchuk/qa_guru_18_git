04d0bbc647899ecf068662ee11e2432ea69aaea3
VeronikaStolyarchuk <veronika.stolyarchuk@yandex.ru>
2026-04-19 14:57:55 +0300

    Update Main.java

--- src/main/java/ru/VeronikaST/Main.java
+++ src/main/java/ru/VeronikaST/Main.java
@@ -3,7 +3,7 @@ package ru.VeronikaST;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static String msg = "Hello qa.guru!!!!!!!!!!!!";
+    private static String msg = "Hello world";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
